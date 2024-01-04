39c9a2543b1654a06c5b47e56400e9b880c84c89
DevAvishArora <138298644+DevAvishArora@users.noreply.github.com>
2024-01-04 16:51:42 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -84,7 +84,6 @@ const userSchema = new mongoose.Schema({
 
 }, { timestamps: true })
 
-//encrypting password before saving
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
         next();
@@ -92,12 +91,10 @@ userSchema.pre('save', async function (next) {
     this.password = await bcrypt.hash(this.password, 10)
 })
 
-// compare user password
 userSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
 
-// return a JWT token
 userSchema.methods.getJwtToken = function () {
     return jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
         expiresIn: 3600
